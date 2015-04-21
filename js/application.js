8dcfe3b5d7e1c7d87b55b619bc7f37a0ecc51c02
Tom Canac <contact@tomcanac.com>
2015-04-21 11:15:39 -0400

    json import nearly functionnal

--- js/application.js
+++ js/application.js
@@ -2,7 +2,6 @@
 App = Ember.Application.create({
     LOG_TRANSITIONS: true
 });
-
 App.ApplicationStore = DS.Store.extend();
 App.ApplicationAdapter = DS.FixtureAdapter.extend();
 
@@ -18,12 +17,8 @@ App.Router.map(function() {
 App.GenerationController = Ember.Controller.extend({
 	actions: {
 		generation: function(){
-
-			// ????
-			var store = App.__container__.lookup('store:main');
-
 			var url = "php/graphCreation.php";
-
+			var store = this.store;
 			data = {
 				numberOfNodes: $('#numberOfNodes').value,
 				complexity: $('#complexity').value
@@ -36,29 +31,47 @@ App.GenerationController = Ember.Controller.extend({
 				success: function(data){
 					var result = $.parseJSON(data);
 					
-					// Save the new graph in the fixtures
-					var id = 1;
-					graph = 1;
+					// Create the graph
+					var currentGraph = store.createRecord('graph', {
+						id:1
+					});
 					
 					// Create the nodes
-					for (var i = result.nodes.length - 1; i >= 0; i--) {
-						store.createRecord('node',{
-							id:id++,
-							links:result.nodes[i].links,
-							graph:graph
+					for (var i = 1; i <= result.nodes.length; i++) {
+						var newNode = store.createRecord('node',{
+							id:i,
+							graph:currentGraph
 						});
-					};
+						// Push the node in the graph's list
+						currentGraph.get('nodes').then(function(nodes){
+							nodes.pushObject(newNode);
+						});
 
-					id=1;
-					// Create the links
-					for (var i = result.edges.length - 1; i >= 0; i--) {
-						store.createRecord('link', {
-							id:id++,
-							nodes: result.edges[i].nodes,
-							weight: result.edges[i].weight
-						});
 					};
 					
+					// Create the links
+					for (var i = 0; i < result.edges.length; i++) {
+						var edge = result.edges[i];
+						var newLink = store.createRecord('link', {
+							id:i+1,
+							weight: edge.weight,
+							graph:currentGraph
+						});
+						// Push the nodes in the link's list
+						for (var j = 0; j < 2; j++) {
+							
+							var theNode = store.find('node', edge.nodes[j]);
+							
+							newLink.get('nodes').then(function(nodeList){
+								nodeList.addObject(theNode);
+							});
+							
+						};
+						// Push the link in the graph's list
+						currentGraph.get('links').then(function(links){
+							links.addObject(newLink);
+						});
+					};
 				},
 				error: function(){
 					alert("ko");
@@ -92,40 +105,38 @@ App.IndexRoute = Ember.Route.extend({
 
 /** Models */
 App.Graph = DS.Model.extend({
-	nodes: DS.hasMany('node', {async:true}),
-	links: DS.hasMany('link', {async:true})
+	nodes: DS.hasMany('node', {async:true, inverse:'graph'}),
+	links: DS.hasMany('link', {async:true, inverse:'graph'})
+});
+
+App.Node = DS.Model.extend({
+	graph: DS.belongsTo('graph', {async:true, inverse:'nodes'})
 });
 
 App.Link = DS.Model.extend({
-	nodes: DS.hasMany('node', {async:true}),
 	weight: DS.attr('number'),
-	graph: DS.belongsTo('graph')
+	nodes: DS.hasMany('node', {async:true, inverse:null}),
+	graph: DS.belongsTo('graph', {async:true, inverse:'links'})
 });
-	
-App.Node = DS.Model.extend({
-	links: DS.hasMany('link'),
-	graph: DS.belongsTo('graph')
-});
-
 
 App.Graph.FIXTURES = [
-	{
-		id: 1,
-		nodes: [1,2,3,4],
-		links: [1,2,3,4]
-	}
+	// {
+	// 	id: 1,
+	// 	nodes: [1,2,3,4],
+	// 	links: [1,2,3,4]
+	// }
 ];
 
 App.Node.FIXTURES = [
-	{ id: 1, graph: 1 },
-	{ id: 2, graph: 1 },
-	{ id: 3, graph: 1 },
-	{ id: 4, graph: 1 }
+	// { id: 1, graph: 1 },
+	// { id: 2, graph: 1 },
+	// { id: 3, graph: 1 },
+	// { id: 4, graph: 1 }
 ];
 
 App.Link.FIXTURES = [
-	{ id: 1, nodes: [1,3], weight: 1, graph: 1 },
-	{ id: 2, nodes: [1,2], weight: 2, graph: 1 },
-	{ id: 3, nodes: [2,3], weight: 7, graph: 1 },
-	{ id: 4, nodes: [4,3], weight: 3, graph: 1 }
+	// { id: 1, nodes: [1,3], weight: 1, graph: 1 },
+	// { id: 2, nodes: [1,2], weight: 2, graph: 1 },
+	// { id: 3, nodes: [2,3], weight: 7, graph: 1 },
+	// { id: 4, nodes: [4,3], weight: 3, graph: 1 }
 ];
