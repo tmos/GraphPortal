6c0c1e968840b741e0b490fb2fbeca6e1772f05e
Spieldy <spieldennerjeremy@gmail.com>
2015-04-23 14:31:55 -0400

    generation done

--- php/graphCreation.php
+++ php/graphCreation.php
@@ -1,57 +1,45 @@
 <?php
-if (isset($_POST['numberOfNodes'])) {
-  $maxN = $_POST['numberOfNodes'];
-} else {
-  $maxN = 6;
-}
 
-$maxE = ($maxN*($maxN-1))/2;
-$minE = $maxN - 1;
+//$maxN = 5;
+//$complexity = 50;
+$maxN = $_POST['numberOfNodes'];
+$complexity = $_POST['complexity'];
 $nodes = array();
+$edges = array();
 
-for($i = 1; $i <= $maxN; $i++) {
-  $nodes[] = array('id' => $i);
-}
-
-// generate random edges
-$e = [];
-for($n = 1; $n <= $maxE || count($e) < $minE; $n++) {
-  $e[] = [rand(1, $maxN), rand(1, $maxN)];
-
-  // remove duplicates and self-loops
-  $dup = [];
-  foreach($e as $i => $v) {
-    if ($v[0] == $v[1]) {
-      unset($e[$i]);
-      $n--;
-    }
-    $d = $v[0].':'.$v[1];
-
-    if (isset($dup[$d])) {
-      unset($e[$i]);
-      $n--;
-    } else {
-      $dup[$d] = true;
-    }
-
-    foreach($e as $a => $j) {
-      if ($j[0] == $v[1] && $j[1] == $v[0]) {
-        unset($e[$a]);
+$isGenerating = true;
+while($isGenerating) {
+  $nodes = array();
+  $edges = array();
+  for($i = 1; $i <= $maxN; $i++) {
+    $nodes[] = array('id' => $i);
+    for($j = 1; $j <= count($nodes); $j++) {
+      if($j != $i) {
+        $choice = rand(0,100);
+        if ($choice < $complexity) {
+          $tempEdge['from'] = $j;
+          $tempEdge['to'] = $i;
+          $tempEdge['weight'] = rand(1,10);
+          $edges[] = $tempEdge;
+        }
       }
     }
   }
+
+  $test = array();
+  $test[] = 1;
+  for($k = 0; $k < count($edges); $k++) {
+    if(in_array($edges[$k]['from'],$test)) {
+      if(!in_array($edges[$k]['to'],$test)) {
+        $test[] = $edges[$k]['to'];
+      }
+    }
+  }
+
+  if (count($test) == $maxN) {
+    $isGenerating = false;
+  }
 }
-
-$edges = array();
-
-foreach($e as $edge) {
-  list($from, $to) = $edge;
-  $tempEdge = array();
-  $tempEdge['nodes'] = array($from, $to);
-  $tempEdge['weight'] = rand(1,10);
-  $edges[] = $tempEdge;
-}
-
 
 $arr = array(
   'size' => $maxN,
